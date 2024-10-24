4ce8d688b81e27f59eb1db9a0dd9603b2fb4d2ae
0xJeu <stephane.jeudy@icloud.com>
2024-10-24 16:11:12 -0500

    Add dealerHand

--- src/main/java/com/pluralsight/Main.java
+++ src/main/java/com/pluralsight/Main.java
@@ -10,6 +10,7 @@ public class Main {
     public static void main(String[] args) {
         Deck deck = new Deck();
         HashMap<String, Hand> players = playerCreation();
+        Hand dealerHand = new Hand();
 
         //Print statement checks deck size.
         System.out.println("Deck size is " + deck.getSize());
@@ -17,10 +18,16 @@ public class Main {
         //Shuffle the deck before dealing
         deck.shuffle();
 
-        // Loop hands dealing the cards to available players in HashMap
+        //Loop handles dealing cards to the dealer
+        for (int i = 0; i < 1; i++) {
+            Card card = deck.deal();
+            dealerHand.deal(card);
+        }
+
+        // Loop handles dealing the cards to available players in HashMap
         for (Map.Entry<String, Hand> entry : players.entrySet()) {
             // deal 2 cards
-            for (int i = 0; i < 2; i++) {
+            for (int i = 0; i < 1; i++) {
                 // get a card from the deck
                 Card card = deck.deal();
                 // deal that card to the hand
@@ -30,6 +37,8 @@ public class Main {
             //Print player's initial hand value
             System.out.printf("%s's hand is worth %d.\n", entry.getKey(), entry.getValue().getValue());
         }
+
+        System.out.printf("%s's hand is worth %d.\n", "Dealer", dealerHand.getValue());
 
 
         //Method to decide whether each player should "hit" (draw a card) or "stay" (keep current hand) based on their current hand
@@ -44,7 +53,7 @@ public class Main {
         }
 
         //Method to find winner
-        String winner = determineWinner(players);
+        String winner = determineWinner(players, dealerHand);
 
         // Conditional to determine what print statement to display at the end
         if (winner.equalsIgnoreCase("Tie")) {
@@ -79,28 +88,31 @@ public class Main {
         return players;
     }
 
-    public static String determineWinner(HashMap<String, Hand> players) {
+    public static String determineWinner(HashMap<String, Hand> players, Hand dealerHand) {
         //Variables needed to determine winner
         int highScore = 0;
         List<String> winner = new ArrayList<>();
         boolean isTied = false;
 
+        // Include dealer's hand in the comparison
+        int dealerValue = dealerHand.getValue();
+        if (dealerValue <= 21) {
+            highScore = dealerValue;
+            winner.add("Dealer");
+        }
+
         //This first loops finds the highest score
         for (Map.Entry<String, Hand> entry : players.entrySet()) {
             int handValue = entry.getValue().getValue();
 
-            if (handValue <= 21 && handValue > highScore) {
-                highScore = handValue;
-            }
-        }
-
-        //The second loops finds the highest score if there are ties between a large group of players
-        for (Map.Entry<String, Hand> entry : players.entrySet()) {
-            String playerName = entry.getKey();
-            int handValue = entry.getValue().getValue();
-
-            if (handValue == highScore) {
-                winner.add(playerName);
+            if (handValue <= 21) {
+                if (handValue > highScore) {
+                    highScore = handValue;
+                    winner.clear();  // Clear previous winners as we have a new high score
+                    winner.add(entry.getKey());
+                } else if (handValue == highScore) {
+                    winner.add(entry.getKey());
+                }
             }
         }
 
